97e156b30619ec60ad415de0d31eddc93cd7ccd7
rlang <rlang@172.17.3.217>
2017-04-10 10:50:55 +0200

    Population

--- flow_permutation_algorithme_memetique/src/flow_permutation_algorithme_memetique/Population.java
+++ flow_permutation_algorithme_memetique/src/flow_permutation_algorithme_memetique/Population.java
@@ -3,6 +3,26 @@ package flow_permutation_algorithme_memetique;
 import java.util.ArrayList;
 
 public class Population {
+	
 	private ArrayList<Solution> pop;
+	private Flowshop instance ; 
+	
 
+	private Population(ArrayList<Solution> pop){
+		this.pop = pop ; 
+	}
+	
+	public Solution aleatgener(){
+		ArrayList<Integer> hasard = new ArrayList<Integer>() ;
+		for (int i = 0 ; i < this.instance.getNbJobs() ; i++){
+			hasard.add(i) ; 
+		}
+		Solution nvsoluce = Solution(this.instance) ;
+		
+		
+		
+	}
 }
+
+	
+	
