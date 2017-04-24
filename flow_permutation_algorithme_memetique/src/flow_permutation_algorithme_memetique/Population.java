12f415036c3439bc54f80019f09a7bbb8e80f9db
rlang <rlang@172.18.103.244>
2017-04-24 10:56:00 +0200

    solution et population

--- flow_permutation_algorithme_memetique/src/flow_permutation_algorithme_memetique/Population.java
+++ flow_permutation_algorithme_memetique/src/flow_permutation_algorithme_memetique/Population.java
@@ -1,27 +1,54 @@
 package flow_permutation_algorithme_memetique;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Population {
 	
 	private ArrayList<Solution> pop;
 	private Flowshop instance ; 
 	
+	
+	public Population(Flowshop instance){
+		this.pop = new ArrayList<Solution>() ;
+		this.instance = instance ;
+	}
 
-	private Population(ArrayList<Solution> pop){
-		this.pop = pop ; 
+	public Population(ArrayList<Solution> pop,Flowshop instance){
+		this.pop = pop ;
+		this.instance = instance ;
 	}
 	
-	public Solution aleatgener(){
-		ArrayList<Integer> hasard = new ArrayList<Integer>() ;
-		for (int i = 0 ; i < this.instance.getNbJobs() ; i++){
-			hasard.add(i) ; 
+	public Population(int nbindividusouhaite,Flowshop instance){
+		Population popu = new Population(instance);
+		for(int i = 0 ; i < nbindividusouhaite ; i++){
+			popu.add(new Solution(instance));
 		}
-		Solution nvsoluce = Solution(this.instance) ;
-		
-		
-		
+		}
+
+	public ArrayList<Solution> getPop() {
+		return pop;
 	}
+	public void setPop(ArrayList<Solution> pop) {
+		this.pop = pop;
+	}
+	public Flowshop getInstance() {
+		return instance;
+	}
+	public void setInstance(Flowshop instance) {
+		this.instance = instance;
+	}
+	
+	public void add(Solution s){
+		this.pop.add(s) ;
+	}
+	
+	
+	
+	
+	
+	
+
 }
 
 	
--- flow_permutation_algorithme_memetique/src/flow_permutation_algorithme_memetique/Solution.java
+++ flow_permutation_algorithme_memetique/src/flow_permutation_algorithme_memetique/Solution.java
@@ -1,10 +1,34 @@
 package flow_permutation_algorithme_memetique;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Solution {
 	
 	private Flowshop instance ; // reference a l'instance 
 	private int[] ordredesjobs ; // donne l'ordre des jobs
 	private int dureetot ; // donne la duree totale suivant l'ordre ordredesjobs
+	
+	
+	// creer une solution aleatoirement 
+	public Solution(Flowshop instance){
+		this.instance = instance ;
+		this.ordredesjobs = new int[this.instance.getNbJobs()];
+		
+		ArrayList<Integer> hasard = new ArrayList<Integer>() ;
+		for (int i = 0 ; i < instance.getNbJobs() ; i++){
+			hasard.add(i) ; 
+		}
+		for (int i = 0 ; i < instance.getNbJobs() ; i++){
+			Random rand = new Random();
+			int tirage = rand.nextInt(instance.getNbJobs());
+			tirage = tirage%(hasard.size()-1) ;
+			this.getOrdredesjobs()[i] = hasard.get(tirage);
+			hasard.remove(tirage);
+		}
+		this.setDureetotal();
+				
+	}
 	
 	
 	public Flowshop getInstance() {
